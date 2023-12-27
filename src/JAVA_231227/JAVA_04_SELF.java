b581d60110ede73c817ba4cbacab929da088e99f
thehapy0103@naver.com <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>
2023-12-27 22:30:17 +0900

    UPDATE

--- src/JAVA_231227/JAVA_04_SELF.java
+++ src/JAVA_231227/JAVA_04_SELF.java
@@ -7,55 +7,63 @@ public class JAVA_04_SELF {
 	public static class Character {
 
 		String name;
-		static int Level;
+		static int HP; // 체력
+		static int Level; // 레벨
 		static int XP; // 경험치
-		int HP; // 체력
 		
-		public Character(String name, int hP) {
+		public Character(String name, int hP, int Level, int XP) {
 			super();
 			this.name = name;
-			HP = hP;
+			hP = 100;
+			Level = 1; // 메서드는 캐릭터의 레벨을 1 증가시키고, 경험치를 초기화해야 함
+			XP = 0;
+			
+		}
+
+		// =============== 캐릭터 생성 줄 ==================
+		// 아룸, 체력, 레벨, 경험치
+		Character ch1 = new Character("군침이", 100, 1, 0);
+		Character ch2 = new Character("뽀로로", 100, 1, 0);
+		// ============================================
+
+	}
+
+	public static void score() { // 승&패 여부 확인
+		Random random = new Random();
+
+		int bt = random.nextInt(2);
+
+		String result = (bt == 1) ? "승리" : "패배";
+
+		if (result == "승리") { // 승리하면 HP는 20 깎이지만 경험치 20 올라감
+			Character.HP -= 20;
+			Character.XP += 50;
+		} else { // 지면 그냥 HP만 깎임
+			Character.HP -= 20; 
 		}
 		
-		// =============== 캐릭터 생성 줄 ==================
-		Character ch1 = new Character("군침이", 100);
-		Character ch2 = new Character("뽀로로", 100);
-		// ============================================
-		
-	}
+		System.out.println(result);
 	
-	
-	public static void levelUp(int Level, int XP) { // 메서드는 캐릭터의 레벨을 1 증가시키고, 경험치를 초기화해야 함
-
-		Level += 1;
-		XP = 0;
-
 	}
 
 	public static void battle() { // 메서드는 다른 캐릭터와 전투를 시뮬레이션하며,
+
 		
-		// 체력을 감소시킴 체력 감소 수치는 어떤경우에도 20(승패 상관없음)
-		// 전투 결과는 랜덤으로 결정.
-		// 1이면 승리 2면 패배로 설정
-		Random random = new Random();
-		
-		boolean bt = random.nextBoolean();
-		
-		String result = (bt = true) ? "승리" : "패배" ;
-		
+
 		// ch1과 ch2가 전투에서 battle 했습니다.
 		// ch1&2의 현재 체력 ch1의HP
 		// random ch?의 레벨업
-		// ch?가 현재 체력이 0 되면 게임 종료
-		// ch1이 레벨업 했습니다. 현재 레벨:3
-		// ch2이 레벨업 했습니다. 현재 레벨:3
 		
 
 	}
 
 	public static void main(String[] args) {
 		
+		score();
 		
+		// ch?가 현재 체력이 0 되면 게임 종료
+		// ch1이 레벨업 했습니다. 현재 레벨:3
+		// ch2이 레벨업 했습니다. 현재 레벨:3
 
 	}
 }
